127beb7967c8e4caf9b9004f1b6b57a7b2825bfa
jimm9Tran@gmail.com <jimm9Tran@gmail.com>
2024-08-18 14:20:52 +0700

    My First Commit To Test

---
+++ learn/b-10/phuongPhapSinh.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std:
+
+    //Giới thiệu:
+        /*
+        Thuật toán sinh là một phương pháp vét cạn được dùng với các bài toán liệt kê
+        hoặc đếm cấu hình, thỏa mãn  các yêu cầu sau:
+            - Có thể xác định được cấu hình đầu tiên và cấu hình cuối cùng.
+            - Tìm được thuật toán để từ cấu hình hiện tại sinh ra cấu hình kế tiếp.
+        
+        Các bài toán phổ biến sử dụng phương pháp sinh:
+            - Liệt kê cấu hình
+            - Liệt kê xâu nhị phân
+            - Liệt kê hoán vị
+            - Liệt kê tập con
+            - Sinh phân hoạch
+        
+        Mã giả:
+            <Bước 1>: Khởi tạo
+                <Khởi tạo cấu hình đầu tiên>
+            <Bước 2>: Lặp
+                While(<Chưa gặp cấu hình cuối cùng>){
+                    <Đưa ra cấu hình hiện tại>
+                    <Sinh ra cấu hình kế tiếp>
+                }
+                
+                <Đưa ra cấu hình cuối cùng>    
+        
+        
+        
+        */
+
+
+int main(){
+    
+
+    return 0;
+}
